5a4cb75ab5882a48c88a7eff19d8c2fa35a4f072
HaiderIrfan62 <99270101+HaiderIrfan62@users.noreply.github.com>
2022-11-09 18:57:22 +0500

    QR code scanner

--- app/src/main/java/com/example/messmanagementsystem/Home_SP.kt
+++ app/src/main/java/com/example/messmanagementsystem/Home_SP.kt
@@ -1,11 +1,19 @@
 package com.example.messmanagementsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home_sp.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class Home_SP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_sp)
+
+        btn_scan.setOnClickListener {
+            val intent = Intent(this, MainQRScanner::class.java)
+            startActivity(intent)
+        }
     }
 }
